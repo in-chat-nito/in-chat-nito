54b220e42e0bd98051632ff2e54c8a5772b42697
Shanjida6 <shanjida.a@gmail.com>
2019-05-07 20:13:32 -0400

    Last CSS styling

--- client/src/App.js
+++ client/src/App.js
@@ -297,10 +297,12 @@ renderChat() {
 
 renderCoursePage() {
   return (
-    <div className = "wrapper">
-    <Logout logOut={this.logOut}/>
-    <ClassList switchToChat={this.switchToChat} courses={this.state.courses} handleRoomClick={this.handleRoomClick} />
+    
+    <div className = "wrapper"> 
+       <Logout logOut={this.logOut}/>
+      <ClassList switchToChat={this.switchToChat} courses={this.state.courses} handleRoomClick={this.handleRoomClick} />
     </div>
+   
   );
 }
 
